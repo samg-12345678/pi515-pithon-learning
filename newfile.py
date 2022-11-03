2801344d5fc5b1ee32fb244fa3f704e4461eb637
Sam Gardner <116313752+samg-12345678@users.noreply.github.com>
2022-11-03 22:00:42 +0000

    started guessing game

--- newfile.py
+++ newfile.py
@@ -1,1 +1,44 @@
+import random
 print("Hello World")
+thing = 52
+x=48
+print(thing+48)
+a, b, c = "1", "2", "3"
+print(a)
+print(b)
+print(c)
+print(a+b+c)
+a=a+b
+print(a)
+yes, no, maybe = 1, 0, 10
+print(yes)
+colors = ["red","green", "blue"]
+print(colors)
+r,g,b=colors
+print(r)
+print(r+g)
+idk=r+ " " +g
+print(idk)
+print(type(idk))
+print(type(r))
+print(r+g)
+print(random.randrange(1,100))
+print((random.randrange(1,1000)/10))
+print(random.randrange(-1,int("100")))
+oops=random.randrange(1,1000)/10
+print(oops)
+print(c)
+print(r[2])
+print(2>2)
+print(2>=2)
+txt = "red is good"
+print("red" in txt)
+print(" ")
+print("green" in txt)
+txt = txt + "green"
+print("green" in txt)
+sound = 20
+color = 10
+
+    
+
